4b19680a40ebe2f1847a601479cceac87e10dcb3
Allan Campos <allan.barrosc@gmail.com>
2024-09-19 17:30:38 -0300

    feat: testes da SecaoService

--- src/test/java/com/magis5/estoquedebebidas/domain/service/SecaoServiceTest.java
+++ src/test/java/com/magis5/estoquedebebidas/domain/service/SecaoServiceTest.java
@@ -2,24 +2,32 @@ package com.magis5.estoquedebebidas.domain.service;
 
 import com.magis5.estoquedebebidas.core.exceptions.SecaoInvalidaException;
 import com.magis5.estoquedebebidas.data.models.SecaoDTO;
+import com.magis5.estoquedebebidas.domain.entities.Secao;
 import com.magis5.estoquedebebidas.domain.enums.TipoBebida;
 import com.magis5.estoquedebebidas.domain.usecase.chains.implementations.MovimentacaoBebidas;
+import com.magis5.estoquedebebidas.domain.validators.implementations.SecaoValidadorChain;
 import jakarta.persistence.EntityManager;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
-//@SpringBootTest
-//@ActiveProfiles("test")
+@SpringBootTest
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@ActiveProfiles("tests")
 class SecaoServiceTest {
 
-    @Mock
+    @Autowired
     private EntityManager manager;
+
+    @Mock
+    private EntityManager entityManager;
 
     @Mock
     private BebidaService bebidaService;
@@ -33,31 +41,120 @@ class SecaoServiceTest {
     @Mock
     private TiposConsultaSecaoService tiposConsultaSecaoService;
 
+    @InjectMocks
+    private SecaoService secaoService;
+
+    @Autowired
+    private SecaoService service;
+
+    @Mock
+    private SecaoValidadorChain validatorChain;
+
     @BeforeEach
     void setUp() {
         MockitoAnnotations.openMocks(this);
     }
-    @InjectMocks
-    private SecaoService secaoService;
 
+    @Order(1)
     @Test
-    void getBySecaoId() {
+    @DisplayName("Criando Seção integrada com base de dados")
+    void deveCriarSecaoComSucesso() {
+        // Dado um SecaoDTO válido
+        SecaoDTO secaoDTO = new SecaoDTO(
+                1, TipoBebida.ALCOOLICA, 500, 50
+        );
+
+        // Quando o método criarSecao é chamado
+        Secao secao = service.criarSecao(secaoDTO);
+
+        // Então o Secao deve ser persistido no banco
+        assertNotNull(secao.getId()); // Verifica se o ID foi gerado
+        assertEquals(1, secao.getNumSecao());
+        assertEquals(TipoBebida.ALCOOLICA, secao.getTipoBebida());
+        assertEquals(secaoDTO.capacidadeMaxima(), secao.getCapacidadeMaxima());
+        assertEquals(secaoDTO.volume(), secao.getVolumeAtual());
+
+        // Verifica se a secao foi persistida de fato no banco de dados
+        Secao secaoPersistida = manager.find(Secao.class, secao.getId());
+        assertNotNull(secaoPersistida);
+        assertEquals(TipoBebida.ALCOOLICA, secaoPersistida.getTipoBebida());
+    }
+    @Order(2)
+    @Test
+    @DisplayName("Consulta integrada com base de dados de uma Seção pelo ID da Seção")
+    void deveRetornarAsInformacoesDaSecaoPeloId() {
+        // Cria uma nova seção e persiste no banco de dados
+
+        // Recupera a seção pelo ID
+        Secao buscaSecao = service.getBySecaoId(1L);
+
+        // Verifica se a seção recuperada não é nula
+        assertNotNull(buscaSecao, "A seção não deve ser nula");
+
+        // Verifica se o ID é igual ao ID da seção criada
+        assertEquals(1L, buscaSecao.getId(), "Os IDs devem ser iguais");
+
+        // Verifica se os demais campos estão corretos
+        assertEquals(1, buscaSecao.getNumSecao(), "Os números da seção devem ser iguais");
+        assertEquals(TipoBebida.ALCOOLICA, buscaSecao.getTipoBebida(), "Os tipos de bebida devem ser iguais");
+        assertEquals(500.0, buscaSecao.getCapacidadeMaxima(), "As capacidades máximas devem ser iguais");
+        assertEquals(50.0, buscaSecao.getVolumeAtual(), "Os volumes atuais devem ser iguais");
     }
 
     @Test
+    @DisplayName("Consulta integrada de uma Seção pelo ID da seção sem Order")
+    void deveRetornarAsInformacoesDaSecaoPeloIdSemOrdemDeExecucao() {
+        // Cria uma nova seção e persiste no banco de dados
+        SecaoDTO secaoDTO = new SecaoDTO(1, TipoBebida.ALCOOLICA, 500, 50);
+        Secao secao = service.criarSecao(secaoDTO);
+
+        // Recupera a seção pelo ID
+        Secao buscaSecao = service.getBySecaoId(1L);
+
+        // Verifica se a seção recuperada não é nula
+        assertNotNull(buscaSecao, "A seção não deve ser nula");
+
+        // Verifica se o ID é igual ao ID da seção criada
+        assertEquals(1L, buscaSecao.getId(), "Os IDs devem ser iguais");
+
+        //Verifica se os demais campos estão corretos
+        assertEquals(secaoDTO.numero(), buscaSecao.getNumSecao(), "Os números da seção devem ser iguais");
+        assertEquals(secaoDTO.tipoBebida(), buscaSecao.getTipoBebida(), "Os tipos de bebida devem ser iguais");
+        assertEquals(secaoDTO.capacidadeMaxima(), buscaSecao.getCapacidadeMaxima(), "As capacidades máximas devem ser iguais");
+        assertEquals(secaoDTO.volume(), buscaSecao.getVolumeAtual(), "Os volumes atuais devem ser iguais");
+    }
+
+    @Test
+    @DisplayName("Criando Seção mockada")
     void criarSecao() {
-        // Configurações necessárias para os mocks retornarem os valores corretos
-        when(tiposConsultaSecaoService.calcularVolumeTotalEstoque(TipoBebida.ALCOOLICA)).thenReturn(100.0);  // Exemplo de configuração do mock
-        // Configura o comportamento do EntityManager, MovimentacaoBebidas, etc., conforme necessário
+        // Arrange
+        SecaoDTO secaoDTO = new SecaoDTO(1, TipoBebida.ALCOOLICA, 100, 50);
+        Secao expectedSecao = Secao.builder()
+                .numSecao(secaoDTO.numero())
+                .tipoBebida(secaoDTO.tipoBebida())
+                .capacidadeMaxima(secaoDTO.capacidadeMaxima())
+                .volumeAtual(secaoDTO.volume())
+                .build();
 
-        // Chama o método de teste
-        double resultado = tiposConsultaSecaoService.calcularVolumeTotalEstoque(TipoBebida.ALCOOLICA);
+        // Simula o comportamento de validação bem-sucedida
+        doNothing().when(validatorChain).validate(secaoDTO);
 
-        // Valida se o resultado é o esperado
-        assertEquals(100.0, resultado);
+        // Simula o comportamento do método persist
+        doNothing().when(entityManager).persist(expectedSecao);
+
+        // Act
+        Secao resultSecao = secaoService.criarSecao(secaoDTO);
+
+        // Assert
+        assertNotNull(resultSecao);
+        assertEquals(expectedSecao.getNumSecao(), resultSecao.getNumSecao());
+        assertEquals(expectedSecao.getTipoBebida(), resultSecao.getTipoBebida());
+        assertEquals(expectedSecao.getCapacidadeMaxima(), resultSecao.getCapacidadeMaxima());
+        assertEquals(expectedSecao.getVolumeAtual(), resultSecao.getVolumeAtual());
+
     }
-
     @Test
+    @DisplayName("Verificando quando um número for zero")
     void deveLancarExcecaoQuandoNumeroSecaoForZero() {
         // Arrange
         SecaoDTO secaoDTO = new SecaoDTO(0, TipoBebida.ALCOOLICA, 100.0, 50.0);
@@ -66,6 +163,20 @@ class SecaoServiceTest {
     }
 
     @Test
-    void retirarBebida() {
+    @DisplayName("Validando retorno de exceção")
+    void testCriarSecao_ValidationError() {
+        // Arrange
+        SecaoDTO secaoDTO = new SecaoDTO(1, TipoBebida.ALCOOLICA, 600, 50);
+
+        // Simula uma exceção de validação
+        doThrow(new SecaoInvalidaException("Houve um erro em uma ou mais regras de validação"))
+                .when(validatorChain).validate(secaoDTO);
+
+        // Act & Assert
+        SecaoInvalidaException thrown = assertThrows(SecaoInvalidaException.class, () -> {
+            secaoService.criarSecao(secaoDTO);
+        });
+
+        assertEquals("secao inválida: Houve um erro em uma ou mais regras de validação", thrown.getMessage());
     }
 }
