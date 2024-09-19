2222ac6cd4b2e44552d54386d7a929170a76c8cb
Allan Campos <allan.barrosc@gmail.com>
2024-09-19 03:31:38 -0300

    feat: adição de outro contrutor

--- src/main/java/com/magis5/estoquedebebidas/core/exceptions/SecaoInvalidaException.java
+++ src/main/java/com/magis5/estoquedebebidas/core/exceptions/SecaoInvalidaException.java
@@ -3,6 +3,10 @@ package com.magis5.estoquedebebidas.core.exceptions;
 import org.springframework.http.HttpStatus;
 
 public class SecaoInvalidaException extends EstoqueBebidasException {
+    public SecaoInvalidaException(String message, String t) {
+        super(HttpStatus.BAD_REQUEST, "secao_invalida", String.format("secao inválida: %s %s", message, t));
+    }
+
     public SecaoInvalidaException(String message) {
         super(HttpStatus.BAD_REQUEST, "secao_invalida", String.format("secao inválida: %s", message));
     }
